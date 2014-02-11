a9d5ec2507a9664bd7781a3674b8c8b4c8a52e49
Dominique MONNET <monnetdo@w51276.ensieta.ecole>
2014-02-11 14:18:12 +0100

    lala

---
+++ scenar.h
@@ -0,0 +1,13 @@
+#ifndef SCENAR_H
+#define SCENAR_H
+
+class scenar
+{
+public:
+    void save(QString f,QString g,QString c);
+    QString initf();
+    QString initg();
+    QString initc();
+};
+
+#endif // SCENAR_H
