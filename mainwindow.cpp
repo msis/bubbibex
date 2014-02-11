263839edce28a4f056d08438ed4df85dea6e45a4
Jean-Baptiste BENEFICE <benefije@w51287.ensieta.ecole>
2014-02-11 15:50:16 +0100

    Commit before merge << simulation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,15 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :  QMainWindow(parent), ui(new Ui::MainW
 }
 
 void MainWindow::Init() {
-    ui->timeBar->setMaximum(ui->tmaxField->value());
+    ui->timeBar->setMaximum(ui->tmaxField->value()-1);
 
     this->show();
 
     // Build the frame
-    double xmin=-10;
-    double xmax=10;
-    double ymin=-10;
-    double ymax=10;
+    double xmin=-20;
+    double xmax=20;
+    double ymin=-20;
+    double ymax=20;
 
     R = new repere(this,ui->graphicsView,xmin,xmax,ymin,ymax);
     R->DrawBox(xmin,xmax,ymin,ymax,QPen(Qt::blue),QBrush(Qt::NoBrush));
@@ -35,7 +35,7 @@ void MainWindow::Init() {
     Function f("f.txt");
     Function g("g.txt");
     Simu = new simulation(f,g);
-    Simu->simuMonteCarlo(R);
+    Simu->simuMonteCarlo(R, 1000);
 
     //Intervalles
 
@@ -53,17 +53,15 @@ void MainWindow::resizeEvent(QResizeEvent* event){
     QMainWindow::resizeEvent(event);
 
     // Build the frame
-    double xmin=-10;
-    double xmax=10;
-    double ymin=-10;
-    double ymax=10;
+    double xmin=-20;
+    double xmax=20;
+    double ymin=-20;
+    double ymax=20;
 
     if(R!=NULL && this->isVisible()){
-        R->Scene->setSceneRect(0,0,ui->graphicsView->geometry().width()-3,ui->graphicsView->geometry().height()-3);
-        R->Clean();
-        R->DrawBox(xmin,xmax,ymin,ymax,QPen(Qt::blue),QBrush(Qt::NoBrush));
-
+        R->Scene->setSceneRect(0,0,ui->graphicsView->geometry().width()-3,ui->graphicsView->geometry().height()-3); 
         drawAll();
+        R->DrawBox(xmin,xmax,ymin,ymax,QPen(Qt::blue),QBrush(Qt::NoBrush));
     }
 }
 
@@ -85,7 +83,11 @@ void MainWindow::on_buttonSimu_stateChanged(int state)
 
 void MainWindow::on_tmaxField_valueChanged(int value)
 {
-    ui->timeBar->setMaximum(ui->tmaxField->value());
+    ui->timeBar->setMaximum(ui->tmaxField->value()-1);
+    R->Clean();
+    Simu->simuMonteCarlo(R, ui->timeBar->maximum());
+    Simu->drawtraj(R);
+
 }
 
 void MainWindow::on_timeBar_valueChanged(int position)
@@ -103,6 +105,7 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::drawAll(){
     double t = ui->timeBar->value();
     ui->curTime->setText(QString::number(t));
+    R->Clean();
 
     //Drawing Paving if box checked
     if(ui->buttonPaving->isChecked()){
@@ -111,7 +114,8 @@ void MainWindow::drawAll(){
 
     //Drawing Simulation if box checked
     if(ui->buttonSimu->isChecked()){
-        //S->drawrob(R, t);
+        Simu->drawtraj(R);
+        Simu->drawrob(R,t);
     }
 
     //Drawing Field if box checked
