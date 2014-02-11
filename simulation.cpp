fc1a33f340860f46c9c7ba6297682134d0edfa2a
qbrefort <quentin.brefort@ensta-bretagne.org>
2014-02-11 11:53:46 +0100

    Classe simulation; Le constructeur initialise les fonctions f&g La fonction simumontecarlo enregistre dans data la trajectoire à partir d'une condition initiale générée aléatoirement. La fonction drawrob permet de tracer le robot à un temps trackbar donné

--- simulation.cpp
+++ simulation.cpp
@@ -1,6 +1,47 @@
 #include "simulation.h"
 
-simulation::simulation()
+simulation::simulation(Function& f, Function& g): fonct_f(f), fonct_g(g)
 {
+    t_trackbar = 0.0;
+    dt = 1.0;
+}
 
+
+void simulation::simuMonteCarlo(repere& R)
+{
+    double x1, x2;
+    x1 = rand()% 20 - 10 ;
+    x2 = rand()% 20 - 10;
+
+    IntervalVector box(3);
+    box[0] = Interval(x1);
+    box[1] = Interval(x2);
+    box[2] = Interval(0.0);
+
+    IntervalVector boxOut(3);
+    boxOut[0] = Interval(x1);
+    boxOut[1] = Interval(x2);
+    boxOut[2] = Interval(0.0);
+
+    for (double i=0 ; i<1000 ;i=i+dt){
+        box[2] = i;
+        data.push_back(boxOut);
+        Interval xPrec = boxOut[0];
+        Interval yPrec = boxOut[1];
+        boxOut = fonct_f.eval_vector(box);
+        R.DrawLine(xPrec.mid(),yPrec.mid(),boxOut[0].mid(),boxOut[1].mid(),QPen(Qt::black));
+    }
+
+}
+
+void simulation::drawrob(repere& R,double t){
+    IntervalVector current;
+    double x,y;
+    t_trackbar = t;
+
+
+    current=data[int(t_trackbar/dt)];
+    y=current[0].mid();
+    x = current[1].mid();
+    R.DrawRobot(x,y,0);
 }
--- simulation.h
+++ simulation.h
@@ -1,10 +1,30 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+#include "repere.h"
+#include "ibex.h"
+
+using namespace ibex;
+
 class simulation
 {
 public:
-    simulation();
+    simulation(Function& f, Function& g);
+    void set_f(Function& f);
+    void set_g(Function& g);
+    void set_t_trackbar(double t);
+
+    void simuMonteCarlo(repere& R);
+    void drawrob(repere& R,double t);
+
+private:
+    Function fonct_f;
+    Function fonct_g;
+    double t_trackbar;
+    double dt;
+    Interval t;
+    vector<IntervalVector> data;
+
 };
 
 #endif // SIMULATION_H
